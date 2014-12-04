499fe5f75ac51581aaeb0f6172f529747e1eaac1
Lee Sanghyuck <shlee322@elab.kr>
2014-12-04 18:25:19 +0900

    회원가입 구현

--- web/main/blueprint.py
+++ web/main/blueprint.py
@@ -16,3 +16,64 @@ def main_login():
         'status': 'failed',
         'message': '존재하지 않는 아이디입니다'
     })
+
+
+@blueprint.route('/_ajax/join', methods=['POST'])
+def main_join():
+    import re
+    data = request.get_json()
+
+    # 아이디 검증
+    username = data['username']
+    if not re.match("^[a-z0-9_.-]{4,64}$", username):
+        return jsonify({
+            'status': 'failed',
+            'message': '아이디는 a-z, 0-9, _, ., -로만 이루어질 수 있으며 4자부터 64자 사이여야 합니다'
+        })
+
+    # 비밀번호 해시
+    password = data['password']
+    import bcrypt
+    password_hash = bcrypt.hashpw(password[:64].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    # 이메일 검증
+    email = data['email']
+    if not re.match("^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", email):
+        return jsonify({
+            'status': 'failed',
+            'message': '이메일 형식이 옳바르지 않습니다'
+        })
+
+    from flask import g
+    # 아이디 체크
+    cur = g.db.cursor()
+    cur.execute('SELECT * FROM `accounts` WHERE `username`=%(username)s;', {
+        'username': username
+    })
+
+    if cur.fetchone():
+        return jsonify({
+            'status': 'failed',
+            'message': '이미 존재하는 아이디입니다'
+        })
+
+    import pymysql
+    try:
+        cur.execute('INSERT INTO `accounts` (`username`, `password`, `email`, `create_time`) ' +
+                    'VALUES (%(username)s, %(password)s, %(email)s, now());', {
+            'username': username,
+            'password': password_hash,
+            'email': email
+        })
+    except pymysql.MySQLError:
+        g.db.rollback()
+        return jsonify({
+            'status': 'failed',
+            'message': '알 수 없는 에러가 발생하였습니다. 잠시후 다시 시도해주세요.'
+        })
+
+    g.db.commit()
+
+    return jsonify({
+        'status': 'succeeded'
+    })
--- web/static/js/main/main.js
+++ web/static/js/main/main.js
@@ -1,3 +1,8 @@
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 peacock.controller('loginController', function($scope, $http) {
     $scope.formData = {};
     $scope.alerts = [];
@@ -8,3 +13,73 @@ peacock.controller('loginController', function($scope, $http) {
         });
     }
 });
+
+peacock.controller('joinController', function($scope, $http) {
+    $scope.formData = {};
+    $scope.alerts = [];
+
+    $scope.submit = function() {
+        $scope.alerts = [];
+
+        if(!$scope.formData.username || $scope.formData.username == '') {
+            $scope.alerts.push({
+                'type':'warning',
+                'msg': '아이디를 입력해주세요'
+            });
+            return;
+        }
+
+        if(!$scope.formData.password || $scope.formData.password == '') {
+            $scope.alerts.push({
+                'type':'warning',
+                'msg': '비밀번호를 입력해주세요'
+            });
+            return;
+        }
+
+        if(!$scope.formData.confirm_password || $scope.formData.confirm_password == '') {
+            $scope.alerts.push({
+                'type':'warning',
+                'msg': '비밀번호 확인 값을 입력해주세요'
+            });
+            return;
+        }
+
+        if(!$scope.formData.email || $scope.formData.email == '') {
+            $scope.alerts.push({
+                'type':'warning',
+                'msg': '이메일을 입력해주세요'
+            });
+            return;
+        }
+
+        if($scope.formData.password != $scope.formData.confirm_password) {
+            $scope.alerts.push({
+                'type':'danger',
+                'msg': '비밀번호와 비밀번호 확인 값이 일치하지 않습니다'
+            });
+            return;
+        }
+
+        if(!validateEmail($scope.formData.email)) {
+            $scope.alerts.push({
+                'type':'danger',
+                'msg': '이메일 형식이 올바르지 않습니다'
+            });
+            return;
+        }
+
+        $http.post('/_ajax/join', $scope.formData).success(function(data){
+            if(data.status == 'failed') {
+                $scope.alerts.push({
+                    'type':'danger',
+                    'msg': data.message
+                });
+                return;
+            }
+
+            //test
+        });
+    }
+});
+
